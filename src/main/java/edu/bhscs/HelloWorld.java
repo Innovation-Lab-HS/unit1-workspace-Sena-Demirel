5670cb8651ae273fa5575ab62652e1f984f72346
Sena-Demirel <2007120@apps.nsd.org>
2024-09-13 18:19:47 +0000

    HelloWorld

---
+++ src/main/java/edu/bhscs/HelloWorld.java
@@ -0,0 +1,9 @@
+class HelloWorld {
+  // all code in Java sits inside a class
+
+  // every Java program needs a public main method
+  public static void main(String[] args) {
+    // for the next while, ALL code you write will live inside main()
+    System.out.println("Hello.");
+  }
+}  
